261d46ee4a0ef74cdad441193f815f19a42effb6
SellerJoke <farustommy@qq.com>
2026-07-19 15:09:05 +0800

    优化bat文件，禁用命令显示

--- SteamGiftsBot.bat
+++ SteamGiftsBot.bat
@@ -1,7 +1,7 @@
-:: 设置控制台编码为UTF-8且不显示本条命令
-chcp 65001 >nul
 :: 禁用命令回显
 @echo off
+:: 设置控制台编码为UTF-8且不显示本条命令
+chcp 65001 > nul
 :: 如果虚拟环境不存在，则创建虚拟环境
 if not exist .venv\Scripts\activate (
     echo "首次运行，创建运行环境中，请稍等..."
