b15c3d4ba8740d39ef5fd470131262b7006aafa6
SellerJoke <farustommy@qq.com>
2026-07-21 11:19:23 +0800

    改进获取Steam Package的代码易读性

--- src/requests/steam_client.py
+++ src/requests/steam_client.py
@@ -85,20 +85,26 @@ class SteamClient:
         name = id_name.name
         name_id = f"<{name}>[{_id}]"
         package_id = str(_id)
+        # 中国区访问的游戏包信息优先提供中国本地化信息，但有些游戏包信息在中国区不可访问，因此切换到美国区访问
         cn_url = SteamClient._PACKAGE_DETAILS_URL.format(package_id=_id, country_code="cn")
         us_url = SteamClient._PACKAGE_DETAILS_URL.format(package_id=_id, country_code="us")
+        should_visit_us_url: bool = False
         response = self._client.get(cn_url)
         if response.is_success:
             data = response.json()
             if not data[package_id]["success"]:
-                response = self._client.get(us_url)
+                should_visit_us_url = True
         else:
+            should_visit_us_url = True
+        if should_visit_us_url:
             response = self._client.get(us_url)
-        if not response.is_success:
-            logger.error(f"获取Steam Package {name_id}详情失败")
-            response.raise_for_status()
-            return None
-        data = response.json()
+            if not response.is_success:
+                logger.error(f"获取Steam Package {name_id}详情失败")
+                response.raise_for_status()
+                return None
+            else:
+                data = response.json()
+        # noinspection PyUnboundLocalVariable
         if not data[package_id]["success"]:
             logger.warning(f"Steam Package {name_id}已删除，详情为空")
             return SteamPackage(_id=_id, name=name, app_infos=[])
